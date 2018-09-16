956a96ddb69f4f268f62870809fdc8dc543c7be4
Helena Graf <Graf.Helena@web.de>
2018-09-16 17:33:35 +0200

    Remove some obsolete classes

--- softwareconfiguration/mlplan/src/de/upb/crc901/automl/metamining/similaritymeasures/IRankMatrixSimilarityComputer.java
+++
@@ -1,8 +0,0 @@
-package de.upb.crc901.automl.metamining.similaritymeasures;
-
-import org.nd4j.linalg.api.ndarray.INDArray;
-
-public interface IRankMatrixSimilarityComputer {
-
-	INDArray computeSimilarityOfRankMatrix(double[][][] performanceValues);
-}
